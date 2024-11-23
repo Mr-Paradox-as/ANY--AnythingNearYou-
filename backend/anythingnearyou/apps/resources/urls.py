994bb513fde0ffc25d12256212e2957c58000c3a
Mr-Paradox-as <shakyabhishekcbr@gmail.com>
2024-11-23 21:56:51 +0530

    few-chnages-resources-page

--- backend/anythingnearyou/apps/resources/urls.py
+++ backend/anythingnearyou/apps/resources/urls.py
@@ -1,7 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from .views import ResourceViewSet
+from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r'resources', ResourceViewSet, basename='resource')
 
 urlpatterns = router.urls
+
+urlpatterns = [
+    path('', include(router.urls)),  # Include the router's URLs
+]
